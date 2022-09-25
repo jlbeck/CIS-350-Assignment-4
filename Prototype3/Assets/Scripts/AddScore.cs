9206cf409600b0fbce63c21c3d9bbd26f16070c3
jlbeck <jlbeck@mail.bradley.edu>
2022-09-24 21:02:07 -0500

    Prototype Part 1

---
+++ Prototype3/Assets/Scripts/AddScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+
+    private UIManager uiManager;
+
+    private bool triggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            uiManager.score++;
+        }
+    }
+
+}
---
+++ Prototype3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //save starting position of the background
+        startPosition = transform.position;
+
+        //set repeatWidth to half of the width of the background using the BoxCollider
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if background is further to left than repeatWidth, reset it to the starting position
+        if (transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+
+    }
+}
